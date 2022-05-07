cab9cb83e852d50808f4392dd11a46f279a4b5ad
John Paul M. Batusan <johnpaul.batusa@bicol-u.edu.ph>
2022-05-08 02:04:50 +0800

    Import components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
-
+import { Col, Form, Button, Row } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Header from './components/header';
+import Governor from './components/governor';
+import ViceGov from './components/viceGovernor';
+import BoardMemFirstDist from './components/boardMemFirstDist';
+import BoardMemSecDist from './components/boardMemSecDist';
+import BoardMemThirdDist from './components/boardMemThirdDist';
+import CongFirstDist from './components/congFirstDist';
+import CongSecDist from './components/congSecDist';
+import CongThirdDist from './components/congThirdDist';
+import MunCouncilor from './components/munDaragaCandidates';
+import Footer from './components/footer';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Governor />
+      <ViceGov />
+      <BoardMemFirstDist />
+      <BoardMemSecDist />
+      <BoardMemThirdDist />
+      <CongFirstDist />
+      <CongSecDist />
+      <CongThirdDist />
+      <MunCouncilor />
+      <Footer />
     </div>
   );
 }
