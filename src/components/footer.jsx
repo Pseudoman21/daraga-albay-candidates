8d0224d323916a7160d28f3b43b4b95625f50ac5
John Paul M. Batusan <johnpaul.batusa@bicol-u.edu.ph>
2022-05-08 02:01:37 +0800

    Add link to email

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -4,7 +4,7 @@ const footer = () => {
     return (<section className="header pt-3">
         <h2>Please vote wisely!</h2>
         <h6><i>This is just to inform and show the voters of the current provincial candidates, I am not tied to any party or person.</i></h6>
-        <h6><i>I apologize for the not-so-good photos, all of that is from public sources. You can email me at johnpaul.batusa@bicol-u.edu.ph if you want to update your photo or you have any correction.</i></h6>
+        <h6><i>I apologize for the not-so-good photos, all of that is from public sources. You can email me at <a href="mailto:johnpaul.batusa@bicol-u.edu.ph">johnpaul.batusa@bicol-u.edu.ph</a> if you want to update your photo or you have any correction.</i></h6>
     </section>)
 }
 
